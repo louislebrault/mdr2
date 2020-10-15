043e2435f10eed14b67fb2a444484c7d3eaa709b
Louis Lebrault <louis.lebrault@gmail.com>
2020-10-15 11:57:39 +0200

    mainLoop is building without error

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,8 @@ module Lib
     ) where
 
 import Network.Socket
+import Network.Socket.ByteString.Lazy (send)
+import Data.ByteString.Lazy.UTF8 (fromString)
 
 address = tupleToHostAddress (127, 0, 0 ,1)
 
@@ -12,4 +14,15 @@ listenMDR = do
     setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
     bind sock (SockAddrInet 4242 address)   -- listen on TCP port 4242.
     listen sock 2                              -- set a max of 2 queued connections
+    mainLoop sock
 
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+    conn <- accept sock     -- accept a connection and handle it
+    runConn conn            -- run our server's logic
+    mainLoop sock           -- repeat
+
+runConn :: (Socket, SockAddr) -> IO ()
+runConn (sock, _) = do
+    send sock (fromString "Hello!\n")
+    close sock
